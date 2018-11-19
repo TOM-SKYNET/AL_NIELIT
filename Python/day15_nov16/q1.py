16ffd20c27405432538fd19c3deb8d7346404dac
TOM-SKYNET <netmail.tom@gmail.com>
2018-11-19 07:24:26 +0530

    day 15 updated

--- Python/day15_nov16/q1.py
+++ Python/day15_nov16/q1.py
@@ -29,7 +29,7 @@ def IsHex(s):
     return False
 
 
-def isDate(s):
+def IsDate(s):
     pat  = re.compile('(\d{2}-\d{2}-\d{4})')
     model = re.match(pat,s)
     if model:
@@ -37,7 +37,7 @@ def isDate(s):
     return False
 
 def IsValidPAssword(s):
-    pat = re.compile(r'^[\w\.\+\-]+\@[\w]+\.[a-z]{2,3}$')
+    pat = re.compile('(?=.*[A-Z].*[A-Z].*)(?=.*[a-z].*[a-z].*)(?=.*[0-9).*[0-9].*)\w{8,16}')
     model = re.match(pat,s)
     if model:
         return True
@@ -51,15 +51,17 @@ def IsValidEmail(s):
     return False
 
 def Normalise(s):
+    return 0
 
 def Normalisen(s,n):
-
+    return 0
 try:
     print ("IsInteger: " , IsInteger('a daa 11'))
     print ("IsFloat: ", IsFloat('dsfs 11.0 aaa'))
     print ("HasVowel: ", HasVowel('apple'))
     print ("isHex: ", IsHex('77094A27E'))
     print ("isDate: ", IsDate('12/02-1999'))
+    print ("IsValidPAssword: ", IsValidPAssword('dd2AcD1pass.com'))
     print ("isValidEmail: ", IsValidEmail('hhsdasdadasasdas.com'))
 
 except Exception as e:

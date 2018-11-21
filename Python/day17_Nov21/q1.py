f2e20c728757276492e013f4d0e0a2bac16f05a6
TOM-SKYNET <netmail.tom@gmail.com>
2018-11-21 15:06:13 +0530

    day17

---
+++ Python/day17_Nov21/q1.py
@@ -0,0 +1,50 @@
+
+# Q2
+import numpy as np
+
+# Q3
+a1 = np.array([1,2,3,4,5])
+print(a1)
+print(a1.dtype)
+
+# Q4
+a2 = np.arange(1,6)
+print(a2)
+print(a2.dtype)
+print("a1 is a2 =", a1 is a2)
+
+# Q5
+a3 = np.ones([2,2,10])
+print(a3)
+
+# Q6
+print("Size:", a3.size)
+
+# Q7. Check and explain the difference between array and asarray
+
+a4 = np.asarray(a3)
+print("a4 is a3 =", a4 is a3)
+a5 = np.array(a3)
+print("a5 is a3 =", a5 is a3)
+
+# Q8. Create an identity mtrix of a given specification.
+
+n =4
+#n = input("Enter Identify Matrix:")
+a6 = np.identity(n)
+print("Identity Matrix:\n",a6)
+print("Size:", a6.size)
+print("Shape:",a6.shape)
+print("Dimension:", a6.ndim)
+print("Data Type:",a6.dtype)
+
+# Q9.Create an array containing 0s inheriting shape from the array created in Q.No.5
+a7 = np.zeros_like(a3)
+print(a7)
+
+
+# Q10.Print the size,shape,dimensions and datatype of all the above arrays. 
+print("Size:", a7.size)
+print("Shape:",a7.shape)
+print("Dimension:", a7.ndim)
+print("Data Type:",a7.dtype)

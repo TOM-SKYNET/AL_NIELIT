461f1f2afa2449c967b06c93533951fb3034b727
TOM-SKYNET <netmail.tom@gmail.com>
2018-12-06 14:34:51 +0530

    NLP updated

---
+++ NLP/Day5_Dec6/q1.py
@@ -0,0 +1,26 @@
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+print ("\nCalculating document similarity scores...")
+
+# Open and read a bunch of files 
+s1="the seas are blue"
+s2="the sun have planets"
+s3="the sun is bright"
+s4="sun has a boy"
+
+# Create a string to use to test the similarity scoring
+
+train_string = 'citizen machane'
+
+# Construct the training set as a list
+train_set = [train_string, s1,s2,s3,s4]
+
+# Set up the vectoriser, passing in the stop words
+tfidf= TfidfVectorizer()
+
+# Apply the vectoriser to the training set
+tfidf_matrix_train = tfidf.fit_transform(train_set)
+
+# Print the score
+print "\nSimilarity Score [*] ",cosine_similarity(tfidf_matrix_train[0:1], tfidf_matrix_train)
---
+++ NLP/Day5_Dec6/q2.py
@@ -0,0 +1,23 @@
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+print ("\nCalculating document similarity scores...")
+
+# Open and read a bunch of files 
+f = open('New.txt')
+doc1 = str(f.read())
+
+# Create a string to use to test the similarity scoring
+train_string = "By these proceedings for judicial review the Claimant seeks to challenge the decision of the Defendant dated the 23rd of May 2014 refusing the Claimant's application of the 3rd of January 2012 for naturalisation as a British citizen"
+
+# Construct the training set as a list
+train_set = [train_string, doc1]
+
+# Set up the vectoriser, passing in the stop words
+tfidf_vectorizer = TfidfVectorizer()
+
+# Apply the vectoriser to the training set
+tfidf_matrix_train = tfidf_vectorizer.fit_transform(train_set)
+
+# Print the score
+print "\nSimilarity Score [*] ",cosine_similarity(tfidf_matrix_train[0:1], tfidf_matrix_train)
